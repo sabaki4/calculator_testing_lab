9a88c7d23fec438fe1a36a446c614035321af218
Saba Kia <skia6589@gmail.com>
2024-03-12 15:41:36 +0000

    subtract function and one summation test

---
+++ calculator.js
@@ -0,0 +1,27 @@
+const sum = function(a, b){
+    return a + b;
+};
+
+const subtract = function(a, b){
+    return a - b;
+};
+
+const multiply = null;
+
+const divide = null;
+
+const modulus = null;
+
+const even = null;
+
+const odd = null;
+
+module.exports = { 
+    sum, 
+    subtract, 
+    multiply, 
+    divide, 
+    modulus, 
+    even, 
+    odd 
+};
